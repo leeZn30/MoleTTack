1c320e41729c3fac726f12aceb35970fbcd45e6f
leeZn30 <55116981+leeZn30@users.noreply.github.com>
2022-05-17 10:35:18 +0900

    unity 설치

---
+++ MoleTTack/Assets/Scripts/02_Game/Mole.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole
+{
+    [Header("SpawnTimes")]
+    [SerializeField] protected float initSpawnTime;
+    [SerializeField] protected float spawnTime;
+
+    [Header("Score")]
+    [SerializeField] protected int score;
+
+    [Header()]
+}
